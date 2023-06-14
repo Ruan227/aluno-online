7efa384d9a970a42a16e0b3f49f9548191635505
Ruan Pascoal <ruansilva.pascoal@gmail.com>
2023-06-14 19:51:23 -0300

    Teste JEST

---
+++ src/models/media.test.js
@@ -0,0 +1,26 @@
+const Nota = require('./nota')
+const nota = new Nota();
+
+describe('Testando média', () => {
+
+    test('Teste MM', () => {
+        expect(nota.mediaCA(6)).toBe('MM - Médio')
+    })
+
+    test('Teste SS', () => {
+        expect(nota.mediaCA(9)).toBe('SS - Superior')
+    })
+
+    test('Teste MI', () => {
+        expect(nota.mediaCA(3.5)).toBe('MI - Médio Inferior')
+    })
+
+    test('Teste II', () => {
+        expect(nota.mediaCA(0.2)).toBe('II - Inferior')
+    }) 
+
+    test('Teste nota inválida', () => {
+        expect(nota.mediaCA(11)).toBe('Nota inválida')
+    }) 
+
+})
