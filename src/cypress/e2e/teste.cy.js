d551d4c6d14b1863f52547a1c813e5913fdc5103
Ruan Pascoal <ruansilva.pascoal@gmail.com>
2023-06-14 21:16:19 -0300

    aulas

--- src/cypress/e2e/teste.cy.js
+++ src/cypress/e2e/teste.cy.js
@@ -1,10 +1,6 @@
 describe('Testando',()=>{
     it('Soma de números',()=>{
-       cy.visit('http://localhost:3000')
-       cy.get('#inputMatricula').type('2023100')
-       cy.get('#inputSenha').type('senha123')
-       cy.get('body > form > button').click()
-       cy.get('#navbarToggler1 > ul.navbar-nav.mr-auto.mt-2.mt-lg-0 > li:nth-child(5) > a').click()
+       cy.visit('http://localhost:3000/requerimentos')
        cy.get('body > div > div > div.card-header > a').click()
        cy.get("#inputDisciplina").select("ADS032 - Construção de Frontend")
        cy.get("#inputProva").select("A1")
